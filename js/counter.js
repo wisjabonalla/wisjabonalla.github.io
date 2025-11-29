0bed96567bd3e0cf823ea564547570881e3481b3
wisjabonalla <wisjabonalla@gmail.com>
2025-11-29 23:54:23 +0800

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,21 @@
-    function startCounter(counterElement, targetValue, duration) {
+function startCounter(counterElement, targetValue, duration) {
         let startValue = 0;
-        let adjustedTarget = targetValue + 1;
-        let increment = adjustedTarget / (duration / 16); 
+        let adjustedTarget = targetValue + 1; 
+        let increment = adjustedTarget / (duration / 24); 
+
+        const needsPlus = counterElement.id === 'projects-counter'; 
 
         const counter = setInterval(() => {
             startValue += increment;
 
             if (startValue >= adjustedTarget) {
                 clearInterval(counter);
-                counterElement.innerText = targetValue + '+';
+                
+                if (needsPlus) {
+                    counterElement.innerText = targetValue + '+';
+                } else {
+                    counterElement.innerText = targetValue;
+                }
             } else {
                 counterElement.innerText = Math.floor(startValue); 
             }
@@ -16,11 +23,12 @@
     }
 
     document.addEventListener('DOMContentLoaded', () => {
-        const counterElement = document.getElementById('projects-counter');
-        const targetValue = 10; 
-
-        if (!counterElement) return; 
-
+        const counterElementProjects = document.getElementById('projects-counter');
+        const counterElementWebsite = document.getElementById('website-counter'); 
+        
+        const targetValueProjects = 10; 
+        const targetValueWebsite = 2; 
+        
         const observerOptions = {
             root: null, 
             rootMargin: '0px',
@@ -30,12 +38,22 @@
         const observer = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    startCounter(entry.target, targetValue, 1500); 
+                    if (entry.target.id === 'projects-counter') {
+                        startCounter(entry.target, targetValueProjects, 1500); 
+                    } else if (entry.target.id === 'website-counter') {
+                        startCounter(entry.target, targetValueWebsite, 1500); 
+                    }
                     
                     observer.unobserve(entry.target); 
                 }
             });
         }, observerOptions);
-
-        observer.observe(counterElement);
+        
+        if (counterElementProjects) {
+            observer.observe(counterElementProjects);
+        }
+        
+        if (counterElementWebsite) { 
+            observer.observe(counterElementWebsite);
+        }
     });
